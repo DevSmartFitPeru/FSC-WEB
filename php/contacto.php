8800e9ecf738dcdf3676cb7c8936fc41a41df355
Michael Balladares <51333297+MichaelBalladaresxd@users.noreply.github.com>
2020-07-06 17:04:51 -0500

    ok

--- php/contacto.php
+++ php/contacto.php
@@ -9,9 +9,11 @@ $contenido = "DE: ".$nombre."\n";
 $contenido .= "EMAIL: ".$email."\n";
 $contenido .= "MENSAJE: ".$mensaje;
 
+$cuerpo=utf8_decode($contenido);
 
 if (!empty($nombre)&&!empty($email)&&!empty($mensaje)) {
-    mail('sistemas@grupofs.com','CONTACTO DESDE LA WEB',$contenido);
+    mail('sistemas@grupofs.com,lab@fscertificaciones.com,
+    fsc@fscertificaciones.com','CONTACTO DESDE LA WEB FSC',$cuerpo);
 
     $data['response']="ok";
 
